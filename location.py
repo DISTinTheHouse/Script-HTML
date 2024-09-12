9b0e4c5f86b75f5558309bea0e97deece1d3ba38
G <134675755+DISTinTheHouse@users.noreply.github.com>
2024-09-12 17:14:01 -0600

    Update location.py

--- location.py
+++ location.py
@@ -2,3 +2,4 @@ from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="Any-user")
 location = geolocator.reverse("25.122222, 55.135497")
 print(location.address)
+#FALTA AGREGAR VIDEO YOUTUBE
